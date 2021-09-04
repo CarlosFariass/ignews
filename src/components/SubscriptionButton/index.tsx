0f7254cf8aa21fb7f22b0e83261541fc635a8404
CarlosFarias <carlos.farias@infracommerce.com.br>
2021-09-04 15:35:03 -0300

    fixing branch and also creating the home view

---
+++ src/components/SubscriptionButton/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+
+
+export function SubscribeButton(){
+  return (
+    <button
+    type="button"
+    className={styles.subscriptionButton}
+    >
+      Subscribe now
+    </button>
+  )
+}
